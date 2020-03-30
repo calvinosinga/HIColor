fdb28b06f0e11fa60ac82a74235c4d7df6d9b6f2
calvinosinga <cosinga@wisc.edu>
2020-03-30 12:34:35 -0500

    Changed the subhalo luminosity run to work better on chtc -> i.e. limiting the amount of requensted resources in exchange for longer runtimes.

--- subhalo_lum.py
+++ subhalo_lum.py
@@ -4,6 +4,7 @@ import sys
 ########## INPUTS #################
 grid = (2048,2048,2048)
 CHUNK = sys.argv[1]
+FIELD = sys.argv[2]
 LUM_MIN, LUM_THRESH = -16, -20 #detection minimum and threshold for breaking into dim/bright bins, from Swanson
 SNAPSHOT = 99
 BOXSIZE = 75 #Mpc/h
@@ -12,10 +13,11 @@ def isred(gr, rband):#color definition as given in Swanson
 ###################################
 f = hp.File('fof_subhalo_tab_0'+str(SNAPSHOT)+'.'+str(CHUNK)+'.hdf5','r')
 has_key = True
-dim = np.zeros(grid)
-bright = np.zeros(grid)
-nondetection = np.zeros(grid)
-magnitude = []
+if FIELD=='magnitude':
+    dat = []
+else:
+    dat = np.zeros(grid)
+
 try:
     pos = f['Subhalo']['SubhaloCM']
     mass = f['Subhalo']['SubhaloMass']
@@ -29,19 +31,15 @@ if has_key:
     for j,b in enumerate(bins):
         rmag = photo[j][5]
         #gmag = photo[j][4]
-        magnitude.append(rmag)
-        if rmag<=LUM_THRESH:
-            bright[b[0],b[1],b[2]] += mass[j]
-        elif rmag<=LUM_MIN:
-            dim[b[0],b[1],b[2]] += mass[j]
+        if FIELD == 'magnitude':
+            dat.append(rmag)
+        if rmag<=LUM_THRESH and FIELD == 'bright':
+            dat[b[0],b[1],b[2]] += mass[j]
+        elif rmag<=LUM_MIN and FIELD == 'dim':
+            dat[b[0],b[1],b[2]] += mass[j]
+        elif FIELD == 'nondetection':
+            dat[b[0],b[1],b[2]] += mass[j]
         else:
-            nondetection[b[0],b[1],b[2]] += mass[j]
-w = hp.File('lumin_'+str(SNAPSHOT)+'_'+str(CHUNK)+'.hdf5', 'w')
-w.create_dataset("nondetection", data=nondetection)
-w.create_dataset("bright", data=bright)
-w.create_dataset("dim", data=dim)
-w.create_dataset("luminosity", data=magnitude)
-print('data size of magnitude')
-print(str(sys.getsizeof(magnitude)))
-print('data size of one of the fields:')
-print(str(sys.getsizeof(bright)))
+            print('incorrect field argument - should be bright, dim, nondetection, or magnitude')
+w = hp.File('lumin_'+str(SNAPSHOT)+'_'+str(CHUNK)+'_' +FIELD+'.hdf5', 'w')
+w.create_dataset(FIELD, data=dat)
