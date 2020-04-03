d489e4da095518f33c1eecd527315b0ef0860d09
calvinosinga <cosinga@wisc.edu>
2020-04-03 18:26:24 -0500

    took away main method in ptl_fields.py

--- ptl_fields.py
+++ ptl_fields.py
@@ -11,22 +11,19 @@ snapshot = 99
 BOXSIZE = 75 #Mpc/h
 ###################################
 
-def main():
-    f= hp.File("snap_0"+str(snapshot)+'.'+str(chunk)+".hdf5",'r')
-    keys = list(f.keys())
-    edges = np.linspace(0,BOXSIZE*1000, grid[0])
-    total = np.zeros(grid)
-    for k in keys:
-        if "Part" in k and "3" not in k:
-            mass = f[k]['Masses']
-            pos = f[k]['Coordinates']
-            bins = np.digitize(pos,edges)
-            for ptl,b in enumerate(bins):
-                total[b[0],b[1],b[2]]+=mass[ptl]
-    w = hp.File('ptl_'+str(snapshot)+'_'+str(chunk)+'.hdf5', 'w')
-    w.create_dataset("mass", data=total)
-
-main()
+f= hp.File("snap_0"+str(snapshot)+'.'+str(chunk)+".hdf5",'r')
+keys = list(f.keys())
+edges = np.linspace(0,BOXSIZE*1000, grid[0])
+total = np.zeros(grid)
+for k in keys:
+    if "Part" in k and "3" not in k:
+        mass = f[k]['Masses']
+        pos = f[k]['Coordinates']
+        bins = np.digitize(pos,edges)
+        for ptl,b in enumerate(bins):
+            total[b[0],b[1],b[2]]+=mass[ptl]
+w = hp.File('ptl_'+str(snapshot)+'_'+str(chunk)+'.hdf5', 'w')
+w.create_dataset("mass", data=total)
 
 
 
