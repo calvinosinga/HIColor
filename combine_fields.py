ed8929bb62b1857a80592feb7783327ab53aab94
calvinosinga <cosinga@wisc.edu>
2020-04-05 22:16:55 -0500

    wrote the combine_fields.py

---
+++ combine_fields.py
@@ -0,0 +1,43 @@
+import numpy as np
+import h5py as hp
+import sys
+first = sys.argv[1]
+second = sys.argv[2]
+grid = (2048,2048,2048)
+
+def get_mass(path):
+    try:
+        f=hp.File(path,'r')
+    except IOError:
+        print('files not found')
+        return np.zeros(grid)
+    else:
+        try:
+            mass = f["mass"]
+        except KeyError:
+            print('mass field not found - creating substitute')
+            return np.zeros(grid)
+        else:
+            return mass
+            
+
+mf = get_mass('ptl_99_'+str(first)+'.hdf5')
+sf = get_mass('ptl_99_'+str(second)+'.hdf5')
+total = np.add(mf,sf)
+w = hp.File('subtotal1_ptl_'+str(first)+'_'+str(second)+'.hdf5','w')
+w.create_dataset("mass",data=total)
+# try:
+#     f = hp.File('ptl_99_'+str(first)+'.hdf5', 'r')
+#     s = hp.File('ptl_99_'+str(second)+'.hdf5', 'r')
+# except IOError:
+#     print('files not found')
+# else:
+#     try:
+#         mf = f['mass']
+#         sf = f['mass']
+#     except KeyError:
+#         print('mass field not found - creating substitute')
+#     else:
+#         total = np.add(mf,sf)
+#         w = hp.File('subtotal1_ptl_'+str(first)+'_'+str(second)+'.hdf5','w')
+#         w.create_dataset("mass",data=total)
