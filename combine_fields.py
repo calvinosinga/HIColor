8f8880d37ecad73464032cba21dcd5e3738b8545
calvinosinga <cosinga@wisc.edu>
2020-04-12 16:20:38 -0500

    altered the implementation of combining 4 fields at once

--- combine_fields.py
+++ combine_fields.py
@@ -2,11 +2,15 @@ import numpy as np
 import h5py as hp
 import sys
 
-first = int(sys.argv[1])
-second = int(sys.argv[2])
-process = str(first)
-first = [str(first*2),str((first*2+1))]
-second = [str(second*2),str((second*2+1))]
+first = sys.argv[1]
+second = sys.argv[2]
+third= sys.argv[3]
+fourth = sys.argv[4]
+fifth= sys.argv[5]
+sixth = sys.argv[6]
+seventh = sys.argv[7]
+eighth= sys.argv[8]
+process = sys.argv[9]
 grid = (2048,2048,2048)
 
 def get_mass(path):
@@ -25,27 +29,16 @@ def get_mass(path):
             print('correctly found the mass')
             return mass
 
-total = get_mass('subtotal1_ptl_99_'+first[0]+'_'+first[1]+'.hdf5')
+total = get_mass('subtotal1_ptl_99_'+first+'_'+second+'.hdf5')
 print(sys.getsizeof(total))
-sf = get_mass('subtotal1_ptl_99_'+second[0]+'_'+second[1]+'.hdf5')
-print(sys.getsizeof(sf))
+sf = get_mass('subtotal1_ptl_99_'+third+'_'+fourth+'.hdf5')
+total = np.add(total,sf,dtype=np.float32)
+sf = get_mass('subtotal1_ptl_99_'+fifth+'_'+sixth+'.hdf5')
+total = np.add(total,sf, dtype=np.float32)
+sf = get_mass('subtotal1_ptl_99_'+seventh+'_'+eighth+'.hdf5')
 total = np.add(total,sf,dtype=np.float32)
 print('finished adding, new file:')
 print(sys.getsizeof(total))
 w = hp.File('subtotal2_ptl_'+process+'.hdf5','w')
 w.create_dataset("mass",data=total)
-# try:
-#     f = hp.File('ptl_99_'+str(first)+'.hdf5', 'r')
-#     s = hp.File('ptl_99_'+str(second)+'.hdf5', 'r')
-# except IOError:
-#     print('files not found')
-# else:
-#     try:
-#         mf = f['mass']
-#         sf = f['mass']
-#     except KeyError:
-#         print('mass field not found - creating substitute')
-#     else:
-#         total = np.add(mf,sf)
-#         w = hp.File('subtotal1_ptl_'+str(first)+'_'+str(second)+'.hdf5','w')
-#         w.create_dataset("mass",data=total)
+
