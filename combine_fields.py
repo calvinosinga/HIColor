885a8eedc1826bf9d4553b51216c228c222074db
calvinosinga <cosinga@wisc.edu>
2020-04-15 12:18:47 -0500

    updating for next combine round

--- combine_fields.py
+++ combine_fields.py
@@ -6,11 +6,8 @@ first = sys.argv[1]
 second = sys.argv[2]
 third= sys.argv[3]
 fourth = sys.argv[4]
-fifth= sys.argv[5]
-sixth = sys.argv[6]
-seventh = sys.argv[7]
-eighth= sys.argv[8]
-process = sys.argv[9]
+
+process = sys.argv[5]
 grid = (2048,2048,2048)
 
 def get_mass(path):
@@ -29,19 +26,19 @@ def get_mass(path):
             print('correctly found the mass')
             return mass
 
-total = get_mass('subtotal1_ptl_'+first+'_'+second+'.hdf5')
+total = get_mass('subtotal2_ptl_'+first+'.hdf5')
 print(sys.getsizeof(total))
-sf = get_mass('subtotal1_ptl_'+third+'_'+fourth+'.hdf5')
+sf = get_mass('subtotal2_ptl_'+second+'.hdf5')
 total = np.add(total,sf,dtype=np.float32)
 del sf
-sf = get_mass('subtotal1_ptl_'+fifth+'_'+sixth+'.hdf5')
+sf = get_mass('subtotal2_ptl_'+third+'.hdf5')
 total = np.add(total,sf, dtype=np.float32)
 del sf
-sf = get_mass('subtotal1_ptl_'+seventh+'_'+eighth+'.hdf5')
+sf = get_mass('subtotal2_ptl_'+fourth+'.hdf5')
 total = np.add(total,sf,dtype=np.float32)
 del sf
 print('finished adding, new file:')
 print(sys.getsizeof(total))
-w = hp.File('subtotal2_ptl_'+process+'.hdf5','w')
+w = hp.File('subtotal3_ptl_'+process+'.hdf5','w')
 w.create_dataset("mass",data=total)
 
