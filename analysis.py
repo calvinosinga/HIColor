19dc57509bf3a460337efc3c702809f503e74d3d
calvinosinga <cosinga@wisc.edu>
2020-04-20 20:36:44 -0500

    forgot to convert field to an overdensity

--- analysis.py
+++ analysis.py
@@ -5,8 +5,15 @@ from Pk_library import Pk
 
 
 f = hp.File('final_ptl_0.hdf5', 'r')
-field = f["mass"][:]
-BOXSIZE = 75.0
+field = f["mass"][:]*1e10 #converting to solar masses/h
+BOXSIZE = 75.0 #Mpc/h
+field=np.divide(field,BOXSIZE**3)# converting to a density
+avg = np.mean(field)
+field= np.divide(field,avg); field = np.add(field,-1)
 pk = Pk(field,BOXSIZE, axis = 0, MAS='CIC')
 tpk = np.transpose([pk.k3D, pk.Pk[:,0]])
 np.savetxt("mass_pk.txt",tpk)
+print(pk.Nmodes1D)
+print(pk.k1D)
+print(pk.Pk1D)
+
