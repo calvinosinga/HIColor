6a06b3a13de89ac421bb3fd067ebe580f30fe9f3
calvinosinga <cosinga@wisc.edu>
2020-05-29 12:46:59 -0500

    wrote analysis.sh and analysis.sub files

---
+++ analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+tar -xzf python27.tar.gz
+tar -xzf packages.tar.gz
+export PATH=$PWD/python/bin:$PATH
+export PYTHONPATH=$PWD/packages
+export HOME=$PWD
+
+cp /staging/cosinga/output/$1.gz ./
+
+gunzip $1.gz
+
+python analysis.py $1 $2 $3
+
+rm $1
