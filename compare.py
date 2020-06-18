4896f82fca90f74bf531f303b887934a5224b4ef
calvinosinga <cosinga@wisc.edu>
2020-06-18 14:09:06 -0500

    getting index error on compare.py?

--- compare.py
+++ compare.py
@@ -5,8 +5,8 @@ import sys
 name = sys.argv[1]
 f = hp.File(name,'r')
 keys = list(f.keys())
-dat = f[keys[0]][:]
-f.close()
+dat = f[keys[0]]
+print(dat.shape)
 mid = int(dat.shape[0]/2.0)
 dat = dat[mid-30:mid+30,:,:]
 dat = np.sum(dat,axis=0)
