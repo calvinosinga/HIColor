b94973b5412a8f3cc8594fd0ae00cc9e1d45fae9
calvinosinga <cosinga@wisc.edu>
2020-06-26 17:37:36 -0500

    fixing error with float32s

--- pk.py
+++ pk.py
@@ -9,9 +9,11 @@ savename = sys.argv[3]
 BOXSIZE = 75.0 #Mpc/h
 f = hp.File(filename,'r')
 field = f[fieldname][:]
+field = field.astype(np.float32)
 if 'flags' in list(f.keys()):
     flags = f['flags']
     print(flags)
+
 field = np.divide(field,BOXSIZE**3) #converts to a density
 avg = np.mean(field)
 field = np.divide(field,avg); field = np.add(field,-1)
