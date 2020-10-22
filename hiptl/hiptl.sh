c7dad6b500147dc6176e47d430408a44dad4d432
Calvin Osinga <cosinga@apophis.astro.umd.edu>
2020-10-22 12:10:37 -0400

    adding the sbatch file, adjusting shell file accordingly.

--- hiptl/hiptl.sh
+++ hiptl/hiptl.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-#SBATCH --share
-#SBATCH --mail-user=cosinga@umd.edu
-#SBATCH --mail-type=ALL
-#SBATCH -t 01:00
-#SBATCH -n 1
-#SBATCH --mem=550
 
 cd /homes/cosinga/lstr
 . ~/.profile
 
 module load python/2.7.8
 
-python /homes/cosinga/HIColor/hiptl/hiptl.py
+python /homes/cosinga/HIColor/hiptl/hiptl.py $1
+
