6043682408841f622d9d66333bcb5235d0e202c3
Calvin Osinga <cosinga@login-2.deepthought2.umd.edu>
2020-12-01 19:29:58 -0500

    fixed keyerror and path in commands.txt

--- hiptl/hiptl.py
+++ hiptl/hiptl.py
@@ -22,7 +22,7 @@ out.write('\n'+"successfully opened the ptlfile\n \n")
 
 mass = ptlfile['PartType0']['Masses'][:] #~50 MB
 pos = ptlfile['PartType0']['CenterOfMass'][:] # ~150 MB
-f_neut_h = hih2file['PartType0']['f_neutral_h'][:] #~100 MB
+f_neut_h = hih2file['PartType0']['f_neutral_H'][:] #~100 MB
 models = ['GD14', 'GK11', 'K13', 'S14']
 w = hp.File(SAVE+'hiptl_99.'+CHUNK+'.hdf5', 'w')
 bins = np.digitize(pos, edges) # ~300 MB
