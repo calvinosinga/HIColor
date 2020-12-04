7e9b446432581c004020b44f832e12a136eaa734
root <root@DESKTOP-KKAMGI0.localdomain>
2020-12-04 11:39:53 -0500

    added some memory stuff to hiptl.py

--- hiptl/hiptl.py
+++ hiptl/hiptl.py
@@ -4,7 +4,7 @@ import numpy as np
 import h5py as hp
 import sys
 CHUNK = sys.argv[1]
-BASE = '/lustre/diemer/illustris/hih2/'
+BASE = '/lustre/diemer/illustris/hih2/' 
 HOME = '/lustre/cosinga/ptl99/'
 SAVE = '/lustre/cosinga/hiptl_output/'
 BOXSIZE = 75000 #kpc/h
@@ -12,7 +12,7 @@ grid = (2048,2048,2048)
 fileno = 448
 field = np.zeros(grid, dtype=np.float32) # 35 GB
 edges = np.linspace(0,BOXSIZE,grid[0]-1)
-out = open('/homes/cosinga/output/hiptl_'+ CHUNK+'.txt', 'w')
+out = open('/homes/cosinga/output/hiptl_'+ CHUNK+'.txt', 'w') # 224MB
 out.write("opening the hih2file for chunk " + CHUNK+'\n')
 hih2file = hp.File(BASE+"hih2_particles_099."+CHUNK+".hdf5", 'r')
 out.write("successfully opened the hih2file")
@@ -27,12 +27,14 @@ models = ['GD14', 'GK11', 'K13', 'S14']
 w = hp.File(SAVE+'hiptl_99.'+CHUNK+'.hdf5', 'w')
 bins = np.digitize(pos, edges) # ~300 MB
 for m in models:
+    out.write("started creating field for "+m)
     h2_frac = hih2file['PartType0']['f_mol_'+m][:] # ~100 MB
     masshi = (1-h2_frac)*f_neut_h*mass # ~100 MB
     masshi = masshi.astype('float32')# decreases size by 50 MB
     for ptl,b in enumerate(bins):
         field[b[0],b[1],b[2]] += masshi[ptl]
     w.create_dataset(m, data=field)
+    out.write("finished creating field for "+m)
 
 dt = time.time() - start
 out.write("the time it took to run: "+str(dt)+" seconds.")
