22c987a5f4729da15fca9e84313318b7c1d0f4b9
Calvin Osinga <cosinga@login-3.deepthought2.umd.edu>
2020-12-27 12:22:37 -0500

    debugging error with wrong filepath

--- write_commands.py
+++ write_commands.py
@@ -2,6 +2,6 @@ f = open("commands.txt", "w")
 fileno = 448
 for i in range(fileno):
     if i==fileno-1:
-        f.write("python3 /lustre/cosinga/hiptl/hiptl.py " +str(i))
+        f.write("python3 /lustre/cosinga/HIColor/hiptl/hiptl.py " +str(i))
     else:
-        f.write("python3 /lustre/cosinga/hiptl/hiptl.py "+str(i)+"\n")
+        f.write("python3 /lustre/cosinga/HIColor/hiptl/hiptl.py "+str(i)+"\n")
